4d1b87f3da90091f7e88787a2ee9c66a4ed017b7
dom <dominic.faryna@gmail.com>
2020-01-08 13:33:17 -0500

    Hello world test

---
+++ src/test/java/DomFaryna/FiveGuysOneRobot/HelloWorld/HelloWorld.java
@@ -0,0 +1,11 @@
+package DomFaryna.FiveGuysOneRobot.HelloWorld;
+
+import org.junit.Test;
+
+public class HelloWorld {
+
+    @Test
+    public void HelloWorldTest(){
+        System.out.println("Hello World");
+    }
+}
