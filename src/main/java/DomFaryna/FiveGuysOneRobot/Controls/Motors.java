7eb8eae4b2783ec8d65ca5a8c720c8316dec6bd5
dom <dominic.faryna@gmail.com>
2020-02-24 10:34:09 -0500

    Refactored motors to use gpio libary. Hopefully this'll work this time

--- src/main/java/DomFaryna/FiveGuysOneRobot/Controls/Motors.java
+++ src/main/java/DomFaryna/FiveGuysOneRobot/Controls/Motors.java
@@ -1,52 +1,49 @@
 package DomFaryna.FiveGuysOneRobot.Controls;
 
-import com.pi4j.wiringpi.Gpio;
+import com.pi4j.io.gpio.*;
 
 // Turns out the motors just use PWM on certain GPIO ports. Sweet and easy
 public class Motors {
     // well, the wiring is detecting by the board, not the
     // pins, so I think its fine to leave it here
-    private final int PWMA1 = 6;
-    private final int PWMA2 = 13;
-    private final int PWMB1 = 20;
-    private final int PWMB2 = 21;
-    private final int D1 = 12;
-    private final int D2 = 26;
+    private final Pin M3 = RaspiPin.GPIO_06;
+    private final Pin M4 = RaspiPin.GPIO_13;
+    private final Pin M1 = RaspiPin.GPIO_20;
+    private final Pin M2 = RaspiPin.GPIO_21;
+    private final Pin PWMA = RaspiPin.GPIO_26;
+    private final Pin PWMB = RaspiPin.GPIO_12;
 
-    private int PWMA;
-    private int PWMB;
-    private int D;
+    private GpioPinDigitalOutput d1;
+    private GpioPinDigitalOutput d2;
+    private GpioPinPwmOutput pwm;
     private boolean forward = true;
 
     public Motors(boolean isRight) {
-        Gpio.pwmSetMode(Gpio.PWM_MODE_BAL);
+        GpioController gpio = GpioFactory.getInstance();
         if(isRight){
-            this.PWMA = PWMA1;
-            this.PWMB = PWMB1;
-            this.D = D1;
+            this.d1 = gpio.provisionDigitalOutputPin(M1);
+            this.d2 = gpio.provisionDigitalOutputPin(M2);
+            this.pwm = gpio.provisionPwmOutputPin(PWMA);
         } else {
-            this.PWMA = PWMA2;
-            this.PWMB = PWMB2;
-            this.D = D2;
+            this.d1 = gpio.provisionDigitalOutputPin(M3);
+            this.d2 = gpio.provisionDigitalOutputPin(M4);
+            this.pwm = gpio.provisionPwmOutputPin(PWMB);
         }
-        Gpio.pinMode(PWMA, Gpio.OUTPUT);
-        Gpio.pinMode(PWMB, Gpio.OUTPUT);
-        Gpio.digitalWrite(PWMA, 1);
-        Gpio.digitalWrite(PWMB, 0);
-        Gpio.pinMode(D, Gpio.PWM_OUTPUT);
+        d1.high();
+        d2.low();
     }
 
     public void setSpeed(double speed){
         if(forward && speed < 0.0){
             forward = false;
-            Gpio.digitalWrite(PWMA, 0);
-            Gpio.digitalWrite(PWMB, 1);
+            d1.low();
+            d2.high();
         } else if(!forward && speed > 0.0){
             forward = true;
-            Gpio.digitalWrite(PWMA, 1);
-            Gpio.digitalWrite(PWMB, 0);
+            d1.high();
+            d2.low();
         }
-        Gpio.pwmWrite(D, (int)(1024 * Math.abs(speed)));
+        pwm.setPwm((int)(1024 * Math.abs(speed)));
     }
 
     public void stop(){
